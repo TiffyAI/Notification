95d13357109efe6916c155b07031b72961af36e7
TiffyAI <quentenmeyer@gmail.com>
2025-06-26 10:48:40 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
     self.registration.showNotification('⏰ Time to Claim Your TIFFY!', {
-      body: 'Click to collect your daily key and boost your bag!',
+      body: 'Click to open your wallet and collect your daily key. You can now boost your wallet with valuable TIFFY tokens!',
       icon: 'https://tiffyai.github.io/On-boarding-/TiffyAI-Token.png',
       badge: 'https://tiffyai.github.io/On-boarding-/TiffyAI-Token.png'
     });
